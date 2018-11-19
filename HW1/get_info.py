ded1fe072e237a1b59caf41e82e86aa3c1daec48
Tommaso Lanciano <tommaso@geosrl.net>
2018-11-19 14:11:38 +0100

    [upd] small updates

--- HW1/get_info.py
+++ HW1/get_info.py
@@ -4,7 +4,8 @@ import utils as ut
 with open("genes_list.txt","r") as f:
     genes = [gene.rstrip() for gene in f.readlines()]
 
+# For each gene quet in the Uniprot DB, and return a tsv with all the results
 with open("results.tsv","w") as results:
     results.write("Entry\tEntry name\tStatus\tProtein names\tGene names\tOrganism\tLength\n")
-    for elem in map(ut.get_info, genes):
+    for elem in map(ut.get_info, genes[:3]):
         results.writelines("%s\n" % line for line in elem[1:])
--- HW1/utils.py
+++ HW1/utils.py
@@ -1,6 +1,8 @@
 import bioservices.uniprot as up
 
+# Return the results of a query of UniProt DB
+#TODO: check which columns has to be taken into account
 def get_info(gene):
     u = up.UniProt()
-    return u.search(gene+"+AND+reviewed:yes", columns="genes(PREFERRED)").split("\n")
+    return u.search(gene+"+AND+reviewed:yes+AND+organism:9606").split("\n")
 
