bfdc650ec1a271b3157e5f84b99d40a92c6c5d52
Tommaso Lanciano <tommaso@geosrl.net>
2018-11-20 15:24:53 +0100

    [upd] added ncbi and other updates

--- HW1/get_info.py
+++ HW1/get_info.py
@@ -1,11 +1,35 @@
 import utils as ut
 
+
 # Initialize the gene list
 with open("genes_list.txt","r") as f:
     genes = [gene.rstrip() for gene in f.readlines()]
 
-# For each gene quet in the Uniprot DB, and return a tsv with all the results
-with open("results.tsv","w") as results:
+
+#### Official Gene Symbol  
+
+# Created with HGNC site tool.
+
+
+#### Uniprot AC + Protein Name
+# TODO: Check whether two genes can have same Uniprot AC, ask whether Protein Name is the one in bold in the Uniprot DB
+# TODO: Modify columns needed in results.write
+
+
+# For each gene query in the Uniprot DB, and return a tsv with all the results
+with open("uniprot.tsv","w") as results:
     results.write("Entry\tEntry name\tStatus\tProtein names\tGene names\tOrganism\tLength\n")
-    for elem in map(ut.get_info, genes[:3]):
-        results.writelines("%s\n" % line for line in elem[1:])
+    for elem in map(ut.get_info, genes):
+        results.writelines("%s\t%s\n" % line for line in elem[1:])
+        
+      
+   
+#### Entrez Gene ID
+
+with open("ncbi.tsv","w") as results:
+    results.write("Gene\tID\n")
+    results.writelines("%s\t%s\n" % line for line in zip(genes,map(ut.query_ncbi, genes)))
+    
+    
+#### Description
+#TODO: Understand what they want
--- HW1/utils.py
+++ HW1/utils.py
@@ -1,8 +1,21 @@
 import bioservices.uniprot as up
+from Bio import Entrez
 
 # Return the results of a query of UniProt DB
 #TODO: check which columns has to be taken into account
 def get_info(gene):
     u = up.UniProt()
-    return u.search(gene+"+AND+reviewed:yes+AND+organism:9606").split("\n")
+    return u.search("gene_exact:"+gene+"+AND+reviewed:yes+AND+organism:9606",
+                    columns = "id, entry name, protein names, genes, comment(FUNCTION)").split("\n")
+    
+
+# Return the results of a query of NCBI DB
+def query_ncbi(gene):
+    
+    Entrez.email = "lanciano.1661409@studenti.uniroma1.it"
+    
+    handle = Entrez.esearch(db= "gene", term = "("+gene+"[Gene Name]) AND \"Homo sapiens\"[porgn] AND (alive[prop])")
+    record = Entrez.read(handle)
+    return record["IdList"]
+    
 
