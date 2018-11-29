eeb2c26366cd7a0146cf348547daff58d421f756
Tommaso Lanciano <tommaso@geosrl.net>
2018-11-29 15:40:13 +0100

    part_4 completed

--- HW1/part_4/create_tables.py
+++ HW1/part_4/create_tables.py
@@ -10,27 +10,30 @@ with open("../seed_genes.txt","r") as f:
 ###### Preprocessing Biogrid
 
 # Load the file
-biogrid = pd.read_csv("../part_3/biogrid.tsv", sep = "\t", nrows = 20)
+biogrid = pd.read_csv("../part_3/biogrid.txt", sep = "\t")
 
 # Take the genes involved in interactions
-unique_genes = list((biogrid["Gene_1"].append(biogrid["Gene_2"])).unique())
+unique_genes = list((biogrid["gene_1"].append(biogrid["gene_2"])).unique())
 
 # Create a dictionary gene:uniprot_ac
 ground_truth = dict(zip(unique_genes,map(ut.query_uniprot, unique_genes)))
 
 #Assign the uniprot_ac according to the gene
-biogrid["interactor_1_uniprot"] = biogrid["Gene_1"].map(ground_truth)
-biogrid["interactor_2_uniprot"] = biogrid["Gene_2"].map(ground_truth)
+biogrid["interactor_1_uniprot"] = biogrid["gene_1"].map(ground_truth)
+biogrid["interactor_2_uniprot"] = biogrid["gene_2"].map(ground_truth)
 
 # Add DB information
 biogrid["database"] = ["BioGrid" for _ in range(biogrid.shape[0])]
 
+biogrid.drop_duplicates(inplace = True)
+
 
 ###### Preprocessing IID
 
-iid = pd.read_csv("../part_3/iid.txt", sep = "\t", usecols = ["Query Symbol", "Partner Symbol", "Query UniProt", "Partner UniProt"], nrows = 20)
+iid = pd.read_csv("../part_3/iid.txt", sep = "\t", usecols = ["Query Symbol", "Partner Symbol", "Query UniProt", "Partner UniProt"])
 iid = iid[["Query Symbol", "Partner Symbol", "Query UniProt", "Partner UniProt"]]
 iid["database"] = ["IID" for _ in range(iid.shape[0])]
+iid.drop_duplicates(inplace = True)
 
 
 
@@ -49,6 +52,8 @@ sgi_iid = iid.loc[iid["interactor_1"].isin(genes) & iid["interactor_2"].isin(gen
 
 sgi = pd.concat([sgi_bio, sgi_iid])
 
+sgi.to_csv("sgi.tsv", sep = "\t", index = False)
+
 
 ### Union Interactome
 
@@ -56,6 +61,7 @@ ui_bio = biogrid.loc[biogrid["interactor_1"].isin(genes) | biogrid["interactor_2
 ui_iid = iid.loc[iid["interactor_1"].isin(genes) | iid["interactor_2"].isin(genes)]
 
 ui = pd.concat([ui_bio, ui_iid])
+ui.to_csv("ui.tsv", sep = "\t", index = False)
 
 
 ### Intersection Interactome
@@ -64,3 +70,5 @@ biogrid["set_genes"] = biogrid.apply(lambda row: frozenset([row.interactor_1, ro
 iid["set_genes"] = iid.apply(lambda row: frozenset([row.interactor_1, row.interactor_2]), axis=1)
 
 ii = pd.merge(biogrid, iid, how='inner', on=["set_genes"])
+
+ii.to_csv("ii.tsv", sep = "\t", index = False)
--- HW1/part_4/utils.py
+++ HW1/part_4/utils.py
@@ -4,6 +4,6 @@ def query_uniprot(gene):
     u = up.UniProt()
     try:
         return u.search("gene_exact:"+gene+"+AND+reviewed:yes+AND+organism:9606", 
-                        columns = "id").split("\n")[1:-1]
+                        columns = "id").split("\n")[1]
     except:
         return "pippo"
